2b3e24187e42c65f37fff0b3ad3ec81579f9bf70
周文童 <zhouwentong@finupgroup.com>
2021-06-07 14:37:04 +0800

    通过快慢指针的方法完成链表中环的检测

--- src/main/java/com/wentong/bytedance/linkedlist/DetectCycle.java
+++ src/main/java/com/wentong/bytedance/linkedlist/DetectCycle.java
@@ -25,6 +25,40 @@ public class DetectCycle {
         return null;
     }
 
+    // 双指针判断环
+    public ListNode detectCycle2(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        // 用死循环的原因是：
+        // 1. 要么无环，fast == null 时 return。
+        // 2. 要么有环，双指针最终会相遇
+        while (true) {
+            if (fast == null || fast.next == null) {
+                return null;
+            }
+            fast = fast.next.next;
+            // 代表 fast 已经遍历结束了，肯定没有环。如果有环，fast 会一直死循环
+            if (fast == null) {
+                return null;
+            }
+            slow = slow.next;
+
+            // 代表快慢指针相遇，代表此时有环
+            if (fast == slow) {
+                break;
+            }
+        }
+
+        fast = head;
+        while (fast != slow) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        return fast;
+    }
+
     public static class ListNode {
         int val;
         ListNode next;
