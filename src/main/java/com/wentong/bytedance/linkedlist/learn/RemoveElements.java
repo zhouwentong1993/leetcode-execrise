d63c3141df9113cb6d2747d1246b74a088bac388
周文童 <zhouwentong@finupgroup.com>
2021-06-17 11:51:32 +0800

    移除链表元素

---
+++ src/main/java/com/wentong/bytedance/linkedlist/learn/RemoveElements.java
@@ -0,0 +1,23 @@
+package com.wentong.bytedance.linkedlist.learn;
+
+public class RemoveElements {
+
+    public static void main(String[] args) {
+        RemoveElements r = new RemoveElements();
+        r.removeElements(new ListNode(1, new ListNode(2, new ListNode(3))), 6);
+    }
+
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        ListNode start = dummy;
+        while (head != null) {
+            if (head.val != val) {
+                start.next = new ListNode(head.val);
+                start = start.next;
+            }
+            head = head.next;
+        }
+        return dummy.next;
+    }
+
+}
