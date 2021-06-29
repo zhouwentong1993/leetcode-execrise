da665cab920f7309b1b8d6ba2cb373723b17c18c
周文童 <zhouwentong@finupgroup.com>
2021-06-29 12:21:20 +0800

    先序 + 中序还原树

---
+++ src/main/java/com/wentong/bytedance/tree/learn/BuildTree.java
@@ -0,0 +1,32 @@
+package com.wentong.bytedance.tree.learn;
+
+import com.wentong.bytedance.tree.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BuildTree {
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || inorder == null) {
+            return null;
+        }
+        Map<Integer, Integer> map = new HashMap<>(((int) (inorder.length / 0.75)) + 1);
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return tree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length - 1, map);
+    }
+
+    private TreeNode tree(int[] preorder, int ps, int pe, int[] inorder, int is, int ie, Map<Integer, Integer> map) {
+        if (pe > is) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[ps]);
+        int rootIndex = map.get(preorder[ps]);
+        root.left = tree(preorder, ps + 1, rootIndex + ps - is, inorder, is, rootIndex - 1, map);
+        root.right = tree(preorder, rootIndex + ps - is + 1, pe, inorder, rootIndex + 1, ie, map);
+        return root;
+    }
+
+}
