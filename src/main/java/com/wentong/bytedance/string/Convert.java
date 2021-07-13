95c1b2461741ff0fd3d09ec384eaa4c119422df4
周文童 <zhouwentong@finupgroup.com>
2021-07-13 15:44:08 +0800

    没写完

---
+++ src/main/java/com/wentong/bytedance/string/Convert.java
@@ -0,0 +1,24 @@
+package com.wentong.bytedance.string;
+
+public class Convert {
+
+    public static void main(String[] args) {
+        boolean ab = new Convert().isValid("-");
+        System.out.println(ab);
+    }
+
+    public int convert(String str) {
+        if (!isValid(str)) {
+            return 0;
+        } else {
+
+        }
+        return 0;
+    }
+
+    private boolean isValid(String str) {
+
+        return true;
+    }
+
+}
