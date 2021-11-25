ca2268a69877787112cfebc7c7330807569218a9
zhouwentong <zhouwentong1993@gmail.com>
2021-11-25 18:28:00 +0800

    二分

---
+++ src/main/java/com/wentong/bytedance/sort/Search.java
@@ -0,0 +1,46 @@
+package com.wentong.bytedance.sort;
+
+public class Search {
+
+    public static void main(String[] args) {
+        System.out.println(new Search().search(new int[]{2,2 }, 3));
+    }
+
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int index = binarySearch(nums, target, 0, nums.length);
+        if (index == -1) {
+            return 0;
+        }
+        int r = index;
+        index --;
+        r++;
+        int count = 1;
+        while (index >= 0 && nums[index] == target) {
+            count++;
+            index--;
+        }
+        while (r < nums.length && nums[r] == target) {
+            count++;
+            r++;
+        }
+        return count;
+    }
+
+    private int binarySearch(int[] nums, int target, int left, int right) {
+        if (left >= right) {
+            return -1;
+        }
+        int mid = (left + right) / 2;
+        int midValue = nums[mid];
+        if (target > midValue) {
+            return binarySearch(nums, target, mid + 1, right);
+        } else if (target < midValue) {
+            return binarySearch(nums, target, left, mid - 1);
+        } else {
+            return mid;
+        }
+    }
+}
